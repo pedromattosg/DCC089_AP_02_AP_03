86ad6f88e6b13ac8fb0c2f9655b045c04fa71165
Pedro Henrique de Mattos Gomes <pedromattosg@hotmail.com>
2025-05-04 13:38:55 -0300

    3. Classe TestResult

--- testing_framework.py
+++ testing_framework.py
@@ -3,10 +3,16 @@ class TestCase:
     def __init__(self, test_method_name):
         self.test_method_name = test_method_name
 
-    def run(self):
+    def run(self, result):
+        result.test_started()
         self.set_up()    # chama método de setup
-        test_method = getattr(self, self.test_method_name)
-        test_method()    # chama método de teste 
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()    # chama método de teste
+        except AssertionError as e:
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            result.add_error(self.test_method_name) 
         self.tear_down() # chama método de teardown 
 
     def set_up(self):
@@ -14,3 +20,28 @@ class TestCase:
 
     def tear_down(self):
         pass
+
+class TestResult:
+
+    RUN_MSG = 'run'
+    FAILURE_MSG = 'failed'
+    ERROR_MSG = 'error'
+
+    def __init__(self, suite_name=None):
+        self.run_count = 0
+        self.failures = []
+        self.errors = []
+
+    def test_started(self):
+        self.run_count += 1
+
+    def add_failure(self, test):
+        self.failures.append(test)
+
+    def add_error(self, test):
+        self.errors.append(test)
+
+    def summary(self):
+        return f'{self.run_count} {self.RUN_MSG}, ' \
+               f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
+               f'{str(len(self.errors))} {self.ERROR_MSG}'

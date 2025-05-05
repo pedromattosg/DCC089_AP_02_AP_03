e99fc2980623616327e167f5b60a8e2c0326e04d
Pedro Henrique de Mattos Gomes <pedromattosg@hotmail.com>
2025-05-04 21:12:33 -0300

    7. Executando Todos os Testes

--- tests.py
+++ tests.py
@@ -144,3 +144,16 @@ class TestLoaderTest(TestCase):
         loader = TestLoader()
         names = loader.get_test_case_names(Test)
         assert names == []
+
+loader = TestLoader()
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
+
+runner = TestRunner()
+runner.run(suite)
